75350bf77538e87e8488b2562ba21c2504c880b2
Sujay Muramalla <sujay.muramalla@gmail.com>
2026-04-08 23:48:42 +0200

    added security groups

--- terraform-aws-basics/main.tf
+++ terraform-aws-basics/main.tf
@@ -31,3 +31,61 @@ resource "aws_subnet" "private" {
 }
 
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "tf-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "tf-public-rt"
+  }
+}
+
+
+resource "aws_route" "public_internet_access" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "public_ec2_sg" {
+  name        = "tf-public-ec2-sg"
+  description = "Security group for Terraform public EC2"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "tf-public-ec2-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.public_ec2_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+
+  from_port = 22
+  to_port   = 22
+  ip_protocol = "tcp"
+
+  description = "SSH from internet for training"
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.public_ec2_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+
+  ip_protocol = "-1"
+
+  description = "Allow all outbound traffic"
+}
+
+
