ced37e77fb5c06c8b19968a36cc8c3d8c823ba6b
Sujay Muramalla <sujay.muramalla@gmail.com>
2026-04-08 20:13:17 +0200

    terraform version fixed

--- terraform-aws-basics/main.tf
+++ terraform-aws-basics/main.tf
@@ -1,11 +1,33 @@
 provider "aws" {
-    region = "eu-central-1"
+  region = "eu-central-1"
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = "10.1.0.0/16"
-        tags = {
-        Name = "tf-yaic-vpc"
-    }
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "tf-yaic-vpc"
+  }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "tf-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.2.0/24"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "tf-private-subnet"
+  }
+}
+
 
